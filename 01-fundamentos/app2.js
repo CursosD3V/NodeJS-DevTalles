bec9a5919d4a9020cb88ad13527095563135acc8
Adelso Guerra <aaguerra@innovacion.gob.sv>
2024-06-12 08:43:00 -0600

    feat: :tada: Typescript
    Trasladar de js a ts

--- 01-fundamentos/app2.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs'); // fs is a core module
-
-const data = fs.readFileSync('README.md', 'utf-8'); // readFileSync is a function in fs module
-
-const newData = data.replace(/React/ig, 'Angular'); // replace is a function in string prototype
-
-fs.writeFileSync('README-Angular.md', newData);
